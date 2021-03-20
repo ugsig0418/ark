0dd9a4ed7d07a5a9a96e1332a1411afc81777112
donipop <minjaebae@gmail.com>
2021-03-21 03:25:12 +0900

    1

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const client = require('cheerio-httpcli');
-client.fetch("http://comic.naver.com/webtoon/weekday.nhn", {}, function (err, $, res, body) {
-var list = $(".list_area .col_selected li"); 
-list.each(function(){
-console.log($(this).find(".title").text()); // 제목을 검색해서 출력한다
-console.log($(this).find("div.thumb a img").attr('src')); //이미지를 검색해서 출력한다
-});
-});
+const http = require('http');
+
+http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
